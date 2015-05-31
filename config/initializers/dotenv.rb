806ce6e11d70fdb12fb12b558ff451ab9157315a
Joshua Book <joshua.book@gmail.com>
2015-05-31 13:18:47 -0700

    fix minor errors in loading dotenv

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,9 +1,7 @@
-puts '#' * 30
-puts 'in initializer'
 if Sinatra::Application.environment == :development
   require 'dotenv'
   [
-    :integration
+    :integration,
     :test
   ].each do |env|
     Dotenv.load "config/dotenv/#{env}.rb"
